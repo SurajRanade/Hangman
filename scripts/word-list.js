7464b09450698583d1887a57f262550322f25337
SurajRanade <136145254+SurajRanade@users.noreply.github.com>
2023-09-08 18:35:24 +0530

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,62 @@
+const wordList = [
+        { 
+        word: "bicycle",
+        hint: "A human-powered vehicle with two wheels."
+        },
+        {
+        word: "sunset",
+        hint: "The daily disappearance of the sun below the horizon."
+        },
+        {
+        word: "coffee",
+        hint: "A popular caffeinated beverage made from roasted coffee beans."
+        }, 
+        {
+        word: "dance",
+        hint: "A rhythmic movement of the body often performed to music."
+        },
+        {
+        word: "painting",
+        hint: "An art form using colors on a surface to create images or expression."
+        },
+        {
+        word: "astronomy",
+        hint: "The scientific study of celestial objects and phenomena."
+         },
+         {
+        word: "football",
+        hint: "A popular sport played with a spherical ball."
+        },
+        {
+         word: "chocolate",
+        hint: "A sweet treat made from cocoa beans."
+        },
+        {
+        word: "butterfly",
+         hint: "An insect with colorful wings and a slender body."
+        },
+        {
+         word: "guitar",
+        hint: "A musical instrument with strings."
+        },
+         {
+        word: "oxygen",
+        hint: "A colorless, odorless gas essential for life."
+         },
+        {
+        word: "mountain",
+        hint: "A large natural elevation of the Earth's surface."
+        },
+        {
+            word: "actor",
+            hint: "a person who works in a movies" 
+        },
+        {
+            word: "building",
+            hint: "a place where people stay" 
+        },
+        {
+            word: "actor",
+            hint: "a person who works in a movies" 
+        },
+]
